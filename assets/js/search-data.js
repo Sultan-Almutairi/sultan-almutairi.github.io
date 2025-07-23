9d555906b0c95ce07b0f78a558f40854190a1aa9
Sultan-Almutairi <38826031+Sultan-Almutairi@users.noreply.github.com>
2025-07-23 03:16:32 +0000

    Deploying to gh-pages from @ Sultan-Almutairi/sultan-almutairi.github.io@06d51c882d96e8f9dddfe6935dedd2c4244959c3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,9 +23,9 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-teaching",
-          title: "teaching",
-          description: "My modules.",
+        },{id: "nav-projects",
+          title: "projects",
+          description: "A growing collection of your cool projects.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
